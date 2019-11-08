cd3832bcc8d15c0acfcccd377005c1820cc2c48a
mustafa-al-shaheri <mustafa.alshaheri@gmail.com>
2019-11-08 13:34:31 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let submit=document.getElementById('submit');
+  submit.addEventListener('click',function(event){
+  let ul=document.getElementById('tasks')
+  let input = document.getElementById("new-task-description").value,
+  li =document.createElement('li'),
+  txt=document.createTextNode(input);
+  li.appendChild(txt);
+  ul.appendChild(li);
+  event.preventDefault();
+  })
+
 });
